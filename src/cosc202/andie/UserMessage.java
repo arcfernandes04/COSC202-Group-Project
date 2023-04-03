8337ebb829edc3b11e660af8d8d48a01a42670e8
Joshua Carter <carjo259@student.otago.ac.nz>
2023-04-04 09:44:16 +1200

    Added error messages to the language support

--- src/cosc202/andie/UserMessage.java
+++ src/cosc202/andie/UserMessage.java
@@ -35,35 +35,37 @@ public class UserMessage {
     public static final int CLOSED_OPTION = 3;
 
     /** The dialog option asking the user whether they would like to overwrite the existing file, or not. */
-    public static final int OVERWRITE_EXISTING_FILE_DIALOG = 10;
+    public static final String OVERWRITE_EXISTING_FILE_DIALOG = "OVERWRITE_EXISTING_FILE_DIALOG";
     /** The dialog option asking the user whether they would like to save and exit, or exit without saving, or cancel. */
-    public static final int SAVE_AND_EXIT_DIALOG = 11;
+    public static final String SAVE_AND_EXIT_DIALOG = "SAVE_AND_EXIT_DIALOG";
     /** The dialog option asking the user whether they would like to save the current file and option the selection,
      * or open the selection without saving the current file, or cancel. */
-    public static final int SAVE_AND_OPEN_DIALOG = 12;
+    public static final String SAVE_AND_OPEN_DIALOG = "SAVE_AND_OPEN_DIALOG";
 
     /** A generic warning to tell the user that an error has occurred. */
-    public static final int GENERIC_WARN = 20;
+    public static final String GENERIC_WARN = "GENERIC_WARN";
     /** A warning to tell the user that the redo stack is empty. */
-    public static final int EMPTY_REDO_STACK_WARN = 21;
+    public static final String EMPTY_REDO_STACK_WARN = "EMPTY_REDO_STACK_WARN";
     /** A warning to tell the user that the undo stack is empty. */
-    public static final int EMPTY_UNDO_STACK_WARN = 22;
+    public static final String EMPTY_UNDO_STACK_WARN = "EMPTY_UNDO_STACK_WARN";
     /** A warning to tell the user that the image file they are trying to open is in an unreadable format. */
-    public static final int INVALID_IMG_FILE_WARN = 23;
+    public static final String INVALID_IMG_FILE_WARN = "INVALID_IMG_FILE_WARN";
     /** A warning to tell the user that the operations file they are trying to open is unreadable. */
-    public static final int INVALID_OPS_FILE_WARN = 24;
+    public static final String INVALID_OPS_FILE_WARN = "INVALID_OPS_FILE_WARN";
     /** A warning to tell the user that no file is currently open. */
-    public static final int NULL_FILE_WARN = 25;
+    public static final String NULL_FILE_WARN = "NULL_FILE_WARN";
     /** A warning to tell the user that the file the program can only open image files. */
-    public static final int NON_IMG_FILE_WARN = 26;
+    public static final String NON_IMG_FILE_WARN = "NON_IMG_FILE_WARN";
     /** A warning to tell the user that a fatal error has occurred. */
-    public static final int FATAL_ERROR_WARN = 27;
+    public static final String FATAL_ERROR_WARN = "FATAL_ERROR_WARN";
     /** A warning to tell the user that the desired file was not found. */
-    public static final int FILE_NOT_FOUND_WARN = 28;
+    public static final String FILE_NOT_FOUND_WARN = "FILE_NOT_FOUND_WARN";
     /** A warning to tell the user that the entered file path is invalid. */
-    public static final int INVALID_PATH_WARN = 29;
+    public static final String INVALID_PATH_WARN = "INVALID_PATH_WARN";
     /** A warning to tell the user that the program does not have permission to open the desired file. */
-    public static final int SECURITY_WARN = 30;
+    public static final String SECURITY_WARN = "SECURITY_WARN";
+    /** A warning to tell the user that the desired language file is missing. */
+    public static final String MISSING_LANG_WARN = "MISSING_LANG_WARN";
 
 
     /**
@@ -85,7 +87,7 @@ public class UserMessage {
      * @return The user's answer in response to the dialog - e.g. they may choose {@code UserMessage.YES_OPTION}
      * when asked shown the dialog for {@code UserMessage.showDialog(UserMessage.SAVE_AND_EXIT_DIALOG)}
      */
-    public static int showDialog(int dialogOption){
+    public static int showDialog(String dialogOption){
         return showDialog(dialogOption, UserMessage.parent);
     }
 
@@ -100,27 +102,27 @@ public class UserMessage {
      * @return The user's answer in response to the dialog - e.g. they may choose {@code UserMessage.YES_OPTION}
      * when asked shown the dialog for {@code UserMessage.showDialog(UserMessage.SAVE_AND_EXIT_DIALOG)}
      */
-    public static int showDialog(int dialogOption, JFrame parent){
+    public static int showDialog(String dialogOption, JFrame parent){
         int result = -1;
         String message = "Are you sure you would like to continue with this action?";
         String title = "ANDIE Dialog Window";
 
         //Dialog for when the user tries to overwrite a file.
-        if(dialogOption == UserMessage.OVERWRITE_EXISTING_FILE_DIALOG){
+        if(dialogOption.equalsIgnoreCase(UserMessage.OVERWRITE_EXISTING_FILE_DIALOG)){
             title = "Overwrite File?";
             message = "Would you like to overwrite the existing file?";
             Object[] possibleValues = new Object[]{"Overwrite", "Cancel"};
             result = JOptionPane.showOptionDialog(UserMessage.parent, message, title,
                     JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE,null,possibleValues,possibleValues[1]);
         }//Dialog for when the user tries to exit without saving changes.
-        else if(dialogOption == UserMessage.SAVE_AND_EXIT_DIALOG){
+        else if(dialogOption.equalsIgnoreCase(UserMessage.SAVE_AND_EXIT_DIALOG)){
             title = "Save and Exit?";
             message = "Would you like to save changes before exiting?";
             Object[] possibleValues = new Object[]{"Save and Exit", "Don't Save", "Cancel"};
             result = JOptionPane.showOptionDialog(UserMessage.parent, message, title, JOptionPane.YES_NO_CANCEL_OPTION,
             JOptionPane.QUESTION_MESSAGE, null, possibleValues, possibleValues[0]);
         }//Dialog for trying to open another file without saving changes.
-        else if (dialogOption == UserMessage.SAVE_AND_OPEN_DIALOG) {
+        else if (dialogOption.equalsIgnoreCase(UserMessage.SAVE_AND_OPEN_DIALOG)) {
             title = "Save Changes?";
             message = "Would you like to save the changes to the current file?";
             Object[] possibleValues = new Object[] { "Save", "Don't Save", "Cancel" };
@@ -146,22 +148,13 @@ public class UserMessage {
      * @param warning The warning message to be displayed.
      * @param parent The parent component for the warning window to appear over.
      */
-    public static void showWarning(int warning, JFrame parent) {
+    public static void showWarning(String warning, JFrame parent) {
         String message;
-        String title = "ANDIE Error Message";
+        String title = Language.getWord("Error_title");
 
-        if(warning == EMPTY_REDO_STACK_WARN) message = "There are currently no operations to redo.";
-        else if(warning == EMPTY_UNDO_STACK_WARN) message = "There are currently no operations to undo.";
-        else if (warning == INVALID_IMG_FILE_WARN) message = "The current image's format is invalid.";
-        else if (warning == INVALID_OPS_FILE_WARN) message = "The image's operation file has been corrupted, although the original image is still intact.";
-        else if (warning == NON_IMG_FILE_WARN) message = "The program cannot operate on non-image files.";
-        else if (warning == NULL_FILE_WARN) message = "No file is currently open.";
-        else if (warning == FATAL_ERROR_WARN) message = "A fatal error has occurred. Please try re-opening the program.";
-        else if(warning == FILE_NOT_FOUND_WARN) message = "That file could not be located.";
-        else if(warning == INVALID_PATH_WARN) message = "The path provided is invalid.";
-        else if(warning == SECURITY_WARN) message = "The program does not have the required permissions to open that file.";
-        else if(warning == GENERIC_WARN) message = "An unexpected error occurred.";
-        else throw new IllegalArgumentException("Invalid warning option.");
+        if(warning.equalsIgnoreCase(MISSING_LANG_WARN)) message = "Fatal language error."; // Can't have this error use Language.getWord() otherwise it will create a loop
+        else message = Language.getWord(warning);
+        //else throw new IllegalArgumentException("Invalid warning option.");
 
         JOptionPane.showMessageDialog(parent, message, title, JOptionPane.INFORMATION_MESSAGE);
     }
@@ -173,7 +166,7 @@ public class UserMessage {
      * 
      * @param warning The warning message to be displayed.
      */
-    public static void showWarning(int warning){
+    public static void showWarning(String warning){
         showWarning(warning, UserMessage.parent);
     }
 
